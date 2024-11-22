e304a90eaba1b74bb777026ccbb077fbd1e37774
Babak-Chalacki <babak.chalaki82@gmail.com>
2024-11-22 19:08:04 +0330

    create a mini project C++

---
+++ app.js
@@ -0,0 +1,3 @@
+let num1 = 1
+var num2 = 1
+const num3 = 3
---
+++ index.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+string sentence = "hello my name is: ";
+int num = 1;
+int main(){
+    if(num == 1){
+        cout << "ok";
+    }
+    else{
+        cout << "not ok";
+    }
+}
